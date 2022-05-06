dfed7cd1cbfcca029510b885bf073f24805a6e80
yuval_styr <yuvalstyr@gmail.com>
2022-05-06 17:38:46 +0300

    🐛   form button

---
+++ app/components/TransactionsForm.tsx
@@ -0,0 +1,91 @@
+import {
+  Button,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+} from "@chakra-ui/react"
+import { Transaction } from "@prisma/client"
+import { Form, useNavigate, useTransition } from "remix"
+import { getOptions } from "~/utils/form"
+
+type IProps = {
+  transactions?: Transaction
+}
+
+export function TransactionsForm(props: IProps) {
+  const { transactions: t } = props
+  let navigate = useNavigate()
+  const transition = useTransition()
+  const isSubmitting = Boolean(transition.submission)
+  function onBack() {
+    navigate(-1)
+  }
+  return (
+    <Form method="post">
+      <label>
+        Type:
+        <Select placeholder=" " name="type" defaultValue={t?.type}>
+          {getOptions("TYPE")}
+        </Select>
+      </label>
+      <label>
+        Action:
+        <Select placeholder=" " name="action" defaultValue={t?.action}>
+          {getOptions("ACTION")}
+        </Select>
+      </label>
+      <label>
+        Owner:
+        <Select placeholder=" " name="owner" defaultValue={t?.owner}>
+          {getOptions("OWNER")}
+        </Select>
+      </label>
+      <label>
+        Amount (🇮🇱 NIS ₪) :
+        <NumberInput
+          precision={2}
+          step={1}
+          name="amount"
+          defaultValue={t?.amount}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </label>
+      <label>
+        Month:
+        <Select placeholder=" " name="month" defaultValue={t?.month}>
+          {getOptions("MONTH")}
+        </Select>
+      </label>
+      <label>
+        Year:
+        <Select placeholder=" " name="year" defaultValue={t?.year}>
+          {getOptions("YEAR")}
+        </Select>
+      </label>
+      <label>
+        Description:
+        <Input
+          name="description"
+          defaultValue={t?.description}
+          placeholder="Description"
+        />
+      </label>
+      <Button type="submit" disabled={isSubmitting}>
+        {isSubmitting ? "Submitting..." : t ? "Update" : "Create"}
+      </Button>
+
+      <Button onClick={onBack} disabled={isSubmitting}>
+        Back
+      </Button>
+    </Form>
+  )
+}
