edf122abfdad4b6cd3bb7404f944027eb2f06d07
yuval_styr <yuvalstyr@gmail.com>
2022-11-05 17:24:46 +0200

    🚀  add table for index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,10 +10,20 @@ export const loader: LoaderFunction = async ({}) => {
 
 export default function StatisticRoute() {
   return (
-    <div>
-      <h1>Welcome to Styr Mansion!!!</h1>
-      <div className="hero-content">
-        <img src="/house.png" />
+    <div className="grid justify-items-center from-primary">
+      <nav className="navbar bg-base-100 w-full sticky top-0 z-30">
+        <div className="font-sans text-lg md:text-3xl">
+          <span className="text-primary">Styr</span>
+          <span className="text-base-content">Mansion</span>
+        </div>
+      </nav>
+      <div className="w-screen h-[20vh] from-primary to-secondary bg-gradient-to-b"></div>
+      <div className="text-primary-content text-2xl font-bold grid grid-cols-2 gap-4 p-6 glass w-9/12 -mt-20">
+        <nav className="navbar  w-full sticky top-0 col-span-2">Dashboard</nav>
+        <div className="bg-base-100 rounded-box p-20 lg:p-32">card</div>
+        <div className="bg-base-100 rounded-box p-20 lg:p-32">card</div>
+        <div className="bg-base-100 rounded-box p-20 lg:p-32">card</div>
+        <div className="bg-base-100 rounded-box p-20 lg:p-32">card</div>
       </div>
     </div>
   )
