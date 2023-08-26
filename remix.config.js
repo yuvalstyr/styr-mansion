947bcb90b41df8507f48bb9f6b2354077ab3236b
yuvalstyrbroad <yuval.styr@gmail.com>
2023-08-26 19:16:31 +0300

    🏗️ move to hmr

--- remix.config.js
+++ remix.config.js
@@ -1,25 +1,17 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "vercel",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: ["**/.*"],
-  sourcemap: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
-  // publicPath: "/build/",
-  tailwind: true,
   future: {
     v2_dev: true,
-    v2_routeConvention: true,
-    v2_meta: true,
     v2_errorBoundary: true,
-    v2_normalizeFormMethod: true,
     v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
   },
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "cjs",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
 }
