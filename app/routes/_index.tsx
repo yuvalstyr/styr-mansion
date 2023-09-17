08f300a2470b7c93b4899b11cd34768fef5ed270
yuvalstyrbroad <yuval.styr@broadcom.com>
2023-09-17 15:54:54 +0300

    💄 new remains table

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,12 +18,17 @@ type unbalanced = {
   remains: string
 }
 
+type totals = {
+  [key: string]: number
+}
+
 export const loader = async () => {
   // current link is /{year}-{month}/transactions
   const { link: currentLinkPath } = getCurrentDatePeriodPath("transactions")
   //  calc remaining for all time
   const summaryMaps = await getTotalsMap()
   const unbalanced: { [key: string]: { [key: string]: unbalanced } } = {}
+  const totals: totals = { [MORAN_RAN]: 0, [TransactionOwner.Yuval]: 0 }
   for (let summary of summaryMaps) {
     const month = summary.months[0]
     const year = summary.year
@@ -33,14 +38,20 @@ export const loader = async () => {
     const ranMoran = styrSummary[MORAN_RAN]
     const yuval = styrSummary[TransactionOwner.Yuval]
     const yuvalRemains = Number(yuval.remains.toFixed(0))
-    const moranRemains = Number(ranMoran.remains.toFixed(0))
-    if (moranRemains) {
+    const ranMoranRemains = Number(ranMoran.remains.toFixed(0))
+    if (yuvalRemains || ranMoranRemains) {
+      if (!unbalanced[yearPeriod]) {
+        unbalanced[yearPeriod] = {}
+      }
+    }
+    if (ranMoranRemains) {
       unbalanced[yearPeriod][MORAN_RAN] = {
         year,
         month,
         owner: MORAN_RAN,
         remains: ranMoran.remains.toFixed(0),
       }
+      totals[MORAN_RAN] += ranMoranRemains
     }
     if (yuvalRemains) {
       unbalanced[yearPeriod][TransactionOwner.Yuval] = {
@@ -49,15 +60,16 @@ export const loader = async () => {
         owner: TransactionOwner.Yuval,
         remains: yuval.remains.toFixed(0),
       }
+      totals[TransactionOwner.Yuval] += yuvalRemains
     }
   }
 
-  return json({ unbalanced, currentLinkPath })
+  return json({ unbalanced, currentLinkPath, totals })
 }
 
 export default function StatisticRoute() {
-  const { unbalanced, currentLinkPath } = useLoaderData<typeof loader>()
-
+  const { unbalanced, currentLinkPath, totals } = useLoaderData<typeof loader>()
+  console.log({ unbalanced, totals })
   return (
     <div className="grid justify-items-center from-primary">
       <NavBar transactionsLink={currentLinkPath} />
@@ -72,8 +84,8 @@ export default function StatisticRoute() {
               <th></th>
               <th>Year</th>
               <th>Period</th>
-              <th>Yuval</th>
               <th>{MORAN_RAN}</th>
+              <th>Yuval</th>
             </tr>
           </thead>
           <tbody>
@@ -82,15 +94,16 @@ export default function StatisticRoute() {
               const yuval = unbalanced[key][
                 TransactionOwner.Yuval
               ] as unbalanced
-              const monthInt = Number(moranRan.month)
+              const monthInt = Number(
+                moranRan?.month !== undefined ? moranRan?.month : 1
+              )
               const period = getMonthValueByName(monthInt)
-              const isDebt = Number(moranRan.remains) < 0
               return (
                 <tr key={uuid()}>
                   <td>
                     <div className="button stat-figure text-secondary">
                       <Link
-                        to={`/${moranRan.year.slice(
+                        to={`/${moranRan?.year.slice(
                           2,
                           4
                         )}-${convertMonthToMonthPeriod(monthInt)}/transactions`}
@@ -99,33 +112,48 @@ export default function StatisticRoute() {
                       </Link>
                     </div>
                   </td>
-                  <td>{moranRan.year}</td>
+                  <td>{moranRan?.year}</td>
                   <td>{period}</td>
-                  <td>{moranRan.owner}</td>
-                  <td className={`${isDebt ? "text-error" : "text-success"}`}>
-                    {moranRan.remains}
+                  <td
+                    className={`${
+                      Number(moranRan?.remains) < 0
+                        ? "text-error"
+                        : "text-success"
+                    }`}
+                  >
+                    {moranRan?.remains ?? 0}
                   </td>
-                  <td>
-                    <div className="button stat-figure text-secondary">
-                      <Link
-                        to={`/${yuval.year.slice(
-                          2,
-                          4
-                        )}-${convertMonthToMonthPeriod(monthInt)}/transactions`}
-                      >
-                        <GoToIcon />
-                      </Link>
-                    </div>
-                  </td>
-                  <td>{yuval.year}</td>
-                  <td>{period}</td>
-                  <td>{yuval.owner}</td>
-                  <td className={`${isDebt ? "text-error" : "text-success"}`}>
-                    {yuval.remains}
+                  <td
+                    className={`${
+                      Number(yuval?.remains) < 0 ? "text-error" : "text-success"
+                    }`}
+                  >
+                    {yuval?.remains ?? 0}
                   </td>
                 </tr>
               )
             })}
+            <tr>
+              <td>Summary</td>
+              <td></td>
+              <td></td>
+              <td
+                className={`${
+                  Number(totals[MORAN_RAN]) < 0 ? "text-error" : "text-success"
+                }`}
+              >
+                {totals[MORAN_RAN]}
+              </td>
+              <td
+                className={`${
+                  Number(totals[TransactionOwner.Yuval]) < 0
+                    ? "text-error"
+                    : "text-success"
+                }`}
+              >
+                {totals[TransactionOwner.Yuval]}
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
